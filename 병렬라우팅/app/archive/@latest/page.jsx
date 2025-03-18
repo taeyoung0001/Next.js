54a89b42c1fd2947b5821e6b2ff6ee199ce93e5a
taeyoung <her020212@naver.com>
2025-03-18 13:00:17 +0900

    [병렬라우팅] 에러, 인터셉트라우팅 정리

--- 병렬라우팅/app/archive/@latest/page.jsx
+++
@@ -1,4 +0,0 @@
-//병렬 라우팅
-export default function LatestNewsPage() {
-  return <h1>latest Page</h1>;
-}
--- 병렬라우팅/app/not-found.jsx
+++
@@ -1,8 +0,0 @@
-export default function NewsNotFoundPage() {
-  return (
-    <div id="error">
-      <h1>Not Found!</h1>
-      <p>해당 기사는 찾을 수 없습니다.</p>
-    </div>
-  );
-}
